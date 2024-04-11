7d3e93442583d3cac525059b121f995298aa227e
lucborges <lucaspy07@gmail.com>
2024-04-10 23:39:57 -0300

    feat: create gift dash

--- server/mocks/giftsMock.js
+++ server/mocks/giftsMock.js
@@ -1,16 +1,59 @@
 const giftsList = [
 	{
-		giftName: 'Janta garantida por 1 mês',
+		name: 'Janta garantida por 1 mês',
 		value: 352.38,
 		image: new Blob()
 	},
 	{
-		giftName: '1 ano de corte de cabelo para o noivo',
+		name: '1 ano de corte de cabelo para o noivo',
 		value: 441.79,
 		image: new Blob()
 	},
 	{
-		giftName: 'Fogão',
+		name: 'Fogão',
+		value: 1258.11,
+		image: new Blob()
+	},
+	{
+		name: 'Janta garantida por 1 mês',
+		value: 352.38,
+		image: new Blob()
+	},
+	{
+		name: '1 ano de corte de cabelo para o noivo',
+		value: 441.79,
+		image: new Blob()
+	},
+	{
+		name: 'Fogão',
+		value: 1258.11,
+		image: new Blob()
+	},{
+		name: 'Janta garantida por 1 mês',
+		value: 352.38,
+		image: new Blob()
+	},
+	{
+		name: '1 ano de corte de cabelo para o noivo',
+		value: 441.79,
+		image: new Blob()
+	},
+	{
+		name: 'Fogão',
+		value: 1258.11,
+		image: new Blob()
+	},{
+		name: 'Janta garantida por 1 mês',
+		value: 352.38,
+		image: new Blob()
+	},
+	{
+		name: '1 ano de corte de cabelo para o noivo',
+		value: 441.79,
+		image: new Blob()
+	},
+	{
+		name: 'Fogão',
 		value: 1258.11,
 		image: new Blob()
 	}
--- src/components/gifts/presentation/index.js
+++ src/components/gifts/presentation/index.js
@@ -2,19 +2,26 @@ import React from 'react';
 import s from './style.module.scss';
 import giftsList from 'server/mocks/giftsMock';
 import Image from 'next/image';
+import { Button } from '@mui/material';
 
 const Gifts = () => {
+	console.log(giftsList);
 	return (
 		<>
 			<div className={s.giftContainer}>
 				<h1 className={s.title}>Lista de presentes</h1>
-				<div className={s.cardsContainer}>
-					<div className={s.giftCard}>
-						{giftsList.map((gift) => {
-							<Image src={gift.image} width={240} height={240}/>;
-						})}
-					</div>
-				</div>
+				<ul className={s.cardContainer}>
+					{giftsList.map((gift) => (
+						<>
+							<li className={s.giftCard}>
+								<Image src={gift.image} width={240} height={240}/>
+								<p className={s.giftName}>{gift.name}</p>
+								<p className={s.giftValue}>{gift.value}</p>
+								<Button variant='contained' className={s.button}>Presentear</Button>
+							</li>
+						</>
+					))}
+				</ul>
 			</div>
 		</>
 	);
