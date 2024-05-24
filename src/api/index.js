bd936bbf41f83b85e9c1cd35de345a870990fe4f
lucborges <lucaspy07@gmail.com>
2024-05-24 13:45:47 -0300

    bugfix: test env

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-// import { config } from 'dotenv';
-// config();
+import { config } from 'dotenv';
+config();
+
+console.log(process.env.WEDDING_SERVICE_URL)
 
 export const http = axios.create({
 	baseURL: "https://my-wedding-service-production.up.railway.app",
